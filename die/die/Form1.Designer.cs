169cc7cccd48001b6966d392ee1bbc35d65ca425
Student <Student@LOG-40517061>
2018-03-20 12:17:52 +1000

    added button

---
+++ die/die/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace die
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnhi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnhi
+            // 
+            this.btnhi.Location = new System.Drawing.Point(241, 234);
+            this.btnhi.Name = "btnhi";
+            this.btnhi.Size = new System.Drawing.Size(75, 23);
+            this.btnhi.TabIndex = 0;
+            this.btnhi.Text = "hi";
+            this.btnhi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 494);
+            this.Controls.Add(this.btnhi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnhi;
+    }
+}
+
